3dbbc67656f9348f8037594178cc75ee783ef2e5
Aman Kumar <avy0219@gmail.com>
2021-06-08 22:13:39 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,138 @@
+console.log("server is starting"); 
+
+var fs = require('fs');
+var data = fs.readFileSync('score.json');
+var afinnFile = fs.readFileSync('afinn.json');
+var score = JSON.parse(data);
+var afinnData = JSON.parse(afinnFile);
+
+
+
+
+const { response, json, request } = require('express');
+var express = require('express');
+var cors = require('cors')
+
+const { stringify } = require('querystring');
+const { finished } = require('stream');
+
+var app = express();
+var server = app.listen(8000, listening);
+
+function listening() {
+    console.log("listening...");
+}
+
+ 
+app.use(cors())
+
+
+var corsOptions = {
+    origin: ['http://127.0.0.1:5500','http://127.0.0.1:5501'],
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+
+app.use(express.static('website'));
+app.use(express.json())
+
+app.post("/data", (request, response) => {
+    var file = request.body;
+    console.log(file.value);
+    console.log("post request recieved in server");
+    reply = {
+        'status': 'success',
+        'data_recieved' : {
+            'key': request.body.key,
+            'value': request.body.value
+        }
+    }
+    score[request.body.key]=request.body.value;
+    try{
+        fs.writeFile('score.json', JSON.stringify(score, null, 2), err => {
+            console.log("writing into the file from post request");
+        });
+    } catch(error){
+        console.error(error);
+    }
+    response.json(reply);
+});
+
+app.get("/search/:flower/:num?", get_sunflower);
+app.get("/all",cors(corsOptions) ,sendall);
+app.get("/add/:flower/:num", addNew);
+
+app.get("/delete/:flower", deleteEntry);
+
+function deleteEntry(request, response) {
+    var data = request.params;
+    var flower = data.flower;
+
+    if(score[flower]!=undefined)
+    {   
+        delete score[flower];
+        fs.writeFile('score.json', JSON.stringify(score, null, 2), finished);
+        function finished(err) {
+            console.log(`deleting ${flower} from database`);
+        }
+    }
+    else    
+        console.log(`element with key "${flower}" not in database`);
+    response.send(score);
+    
+}
+
+function addNew(request, response) {
+    
+    var data = request.params;
+    var flower = data.flower;
+    var num = data.num;
+    var reply = {
+        "word": flower,
+        "status": "successful"
+    };
+    score[flower] = Number(num);
+    try {
+        fs.writeFile('score.json' , JSON.stringify(score, null, 2), err => {
+            console.log("writing into the file\n");
+        });
+       
+    } catch (error) {
+        response.send("error while writing into file");   
+    }
+  
+    response.send(reply); 
+    
+
+}
+
+function sendall(request, response) {
+    
+    
+    var head = response['req']['headers']['origin'];
+    console.log("===========================================================================");
+    console.log(head);
+    console.log("===========================================================================");
+
+
+    reply = {
+        "score": score,
+        "afinn data":afinnData
+    }
+
+    response.send(reply);
+}
+
+function get_sunflower(request, response) {
+    
+    var data=request.params;
+    var num =data.num ;
+    if(!num)
+        num=1;
+    var reply='';
+    for(var i=0;i<num;i++)
+        reply+=`<h1>I love ${data.flower} ${i}\n</h1>`;
+
+    response.send(reply);
+
+}
---
+++ website/script.js
@@ -0,0 +1,40 @@
+
+function setup() {
+    console.log("running");
+    
+    fetch('/all/').then(response => {
+        console.log(response);
+        return (response.json());
+    }).then(json => {
+        console.log(json);
+        document.getElementById("123").innerHTML=JSON.stringify(json, null, 2);
+        console.log(json);
+    });    
+
+    window.onload = foo => {
+    var button = document.getElementById("submit");
+    button.onclick = addElement ;
+
+    function addElement() {
+        var word = document.getElementById("word").value;
+        var num = document.getElementById("value").value;
+        fetch("/add/"+word+"/"+num).then(response => 
+            {
+                document.getElementById("status").innerHTML = JSON.stringify(response.json(), null , 2);
+                console.log(response);
+                return response.json();
+            });
+       
+    }
+
+    }
+}
+
+
+
+
+
+
+
+
+setup();
